2cd25b3e40885f04bcfcd8de47475f1985459dcd
S7R1D3R <joaquimtd.info@gmail.com>
2018-04-15 23:47:49 +0100

    Finished the authentication part, users can now log in with Google OAuth and logout. Cookies are implemented.

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 const express = require("express"),
-      app     = express(),
-      authRoutes = require("./routes/authRoutes"),
+      cookieSession = require("cookie-session"),
+      passport = require("passport"),
       keys = require("./config/keys"),
       mongoose = require("mongoose");
 
-      // Models
-      require("./models/User");         // User
+// Models
+require("./models/User");         // User
 
-      // Google OAuth Configuration
-      require("./services/passport");
+// Google OAuth Configuration
+require("./services/passport");
 
-      // Authentication Routes
-      require("./routes/authRoutes")(app);
+// Connect to Database
+mongoose.connect(keys.mongoURI);
 
-      // Connect to Database
-      mongoose.connect(keys.mongoURI);
+const app = express();
 
+// Enable Cookie Sessions
+app.use(
+    cookieSession({
+
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Authentication Routes
+require("./routes/authRoutes")(app);
 
 app.listen(3000, () => console.log("Server started..."));
